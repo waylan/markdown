10ca48fecda3d82be1b7e2458cdf4852b584686e
Waylan Limberg <waylan.limberg@icloud.com>
2020-07-17 11:25:08 -0400

    Use a more permissive HTML parser.

--- markdown/extensions/md_in_html.py
+++ markdown/extensions/md_in_html.py
@@ -19,6 +19,73 @@ from ..blockprocessors import BlockProcessor
 from .. import util
 import re
 import xml.etree.ElementTree as etree
+from html.parser import HTMLParser
+
+
+class HTMLTreeBuilder(HTMLParser):
+    """ Parser a string of HTML into an ElementTree object. """
+
+    def reset(self):
+        """Reset this instance.  Lose all unprocessed data."""
+        self.stack = []
+        self.treebuilder = etree.TreeBuilder(insert_comments=True, insert_pis=True)
+        super().reset()
+
+    def close(self):
+        """ Flush the buffer and return the toplevel element as an etree Element instance. """
+        super().close()
+        return self.treebuilder.close()
+
+    def handle_starttag(self, tag, attrs):
+        attribs = dict()
+        for key, value in attrs:
+            # Valueless attr (`<tag checked>`) results in `('checked', None)`. Convert to `{'checked': 'checked'}`.
+            attribs[key] = value if value is not None else key
+        self.stack.append(tag)
+        self.treebuilder.start(tag, dict(attribs))
+
+    def handle_endtag(self, tag):
+        if tag in self.stack:
+            while self.stack:
+                # Close any unclosed children first, then this element
+                item = self.stack.pop()
+                self.treebuilder.end(item)
+                if item == tag:
+                    break
+        else:
+            # Treat orphan closing tag as an empty tag.
+            self.handle_startendtag(tag, {})
+
+    def handle_data(self, data):
+        self.treebuilder.data(data)
+
+    def handle_pi(self, data):
+        self.treebuilder.pi(data)
+
+    def handle_comment(self, data):
+        self.treebuilder.comment(text)
+
+    def handle_entityref(self, name):
+        self.treebuilder.data('&{};'.format(name))
+
+    def handle_charref(self, name):
+        self.treebuilder.data('&#{};'.format(name))
+
+    def handle_decl(decl):
+        # TODO: Explore using a custom treebuilder which supports a `doctype` method.
+        # See https://docs.python.org/3/library/xml.etree.elementtree.html#xml.etree.ElementTree.TreeBuilder.doctype
+        self.treebuilder.data('<!{}>'.format(decl))
+
+    def unknown_decl(self, data):
+        end = ']]>' if data.startswith('CDATA[') else ']>'
+        self.treebuilder.data('<![{}{}'.format(data, end))
+
+
+def parse_html(data):
+    """ Parser a string of HTML and return the toplevel element as an etree Element instance. """
+    parser = HTMLTreeBuilder(convert_charrefs=False)
+    parser.feed(data)
+    return parser.close()
 
 
 class MarkdownInHtmlProcessor(BlockProcessor):
@@ -48,12 +115,12 @@ class MarkdownInHtmlProcessor(BlockProcessor):
         is set on the element or `markdown="0", then the text content is set to an
         AtomicString, so that no inlne processing will happen either.
         """
-        md_attr = element.attrib.pop('markdown', 0)
+        md_attr = element.attrib.pop('markdown') if 'markdown' in element.attrib else '0'
         if md_attr not in ['0', 'span'] and len(element):
             # handle children
             for child in list(element):
                 self.parse_element_content(child)
-        elif md_attr == 'block' or (md_attr == '1' and element.tag not in self.span_tags):
+        elif md_attr == 'block' or (md_attr in ['1', 'markdown'] and element.tag not in self.span_tags):
             # No children, parse text content
             block = element.text
             element.text = ''
@@ -66,7 +133,7 @@ class MarkdownInHtmlProcessor(BlockProcessor):
         blocks.pop(0)
         raw_html = self.parser.md.htmlStash.rawHtmlBlocks[self.index]
 
-        et_el = etree.fromstring(raw_html)
+        et_el = parse_html(raw_html)
         self.parse_element_content(et_el)
         parent.append(et_el)
 
